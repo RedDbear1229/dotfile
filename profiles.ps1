425a386d0118028d715da3d4857ed6eaaf72e2a3
RedDbear1229 <109574753+RedDbear1229@users.noreply.github.com>
2025-02-27 09:43:00 +0900

    Update profiles.ps1

--- profiles.ps1
+++ profiles.ps1
@@ -1,10 +1,19 @@
-Import-Module PSReadLine
+# set PowerShell to UTF-8
+[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
-Set-Alias ls lsd
-$OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
-
+# Shell to use Starship
 Invoke-Expression (&starship init powershell)
 
-
-
+# Import Modules
+Import-Module PSReadLine
+Import-Module -Name Terminal-Icons
 Import-Module PoShFuck
+Import-Module PSFzf
+
+# PSReadLine
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+# Fzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
